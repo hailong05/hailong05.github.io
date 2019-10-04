cde456efca02cd1360f17b7af15981c5dfd065bd
hailong05 <hailong14a97@gmail.com>
2019-10-05 00:04:27 +0700

    upload DemoCalCulator

---
+++ index.ios.js
@@ -0,0 +1,9 @@
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+} from 'react-native';
+
+import Calculator from './src/component/calculator'
+
+AppRegistry.registerComponent('DemoCalCulator', () => Calculator);
